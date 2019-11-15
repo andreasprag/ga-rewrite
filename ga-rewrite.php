5c2e8e6afbce98a382742e6bb1cdc13d9bb4044a
Andreas <andreasprag@gmail.com>
2019-11-15 06:26:32 +0100

    first commit

---
+++ ga-rewrite.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: Analytics Cookie Rewrite
+ * Plugin URI: https://www.linkedin.com/in/andreasprag/
+ * Description: Rewrite your Google Analytics cookie from Wordpress to circumvent ITP. You need to set 'cookieUpdate' to false on all your Google Analytics tags for this method to work.
+ * Version: 1.0
+ * Author: Andreas Prag
+ * Author URI: https://www.linkedin.com/in/andreasprag/
+ *
+ */
+function ga_rewrite() {
+	
+    if (!isset($_COOKIE['ga_rewrite'])) {
+		$domain = $_SERVER['HTTP_HOST'];
+        if (isset($_COOKIE['_ga'])) {
+            $ga = $_COOKIE['_ga'];
+        }
+		else{
+			$random = str_pad(mt_rand(1,9999999),7,'0',STR_PAD_LEFT);
+			$timestamp = time();
+			$ga = "GA1.2." . $random . "." . $timestamp;
+			
+		}
+        if (isset($_GET["_ga"])) {
+            $ga_param = $_GET["_ga"];
+            if ($ga_param == $ga) {
+                setcookie('_ga', $ga, time() + 63113904, '/', '.'.$domain);
+                setcookie('ga_rewrite', $ga, time() + 604800, '/', '.'.$domain);
+            }
+        } else {
+            setcookie('_ga', $ga, time() + 63113904, '/', '.'.$domain);
+            setcookie('ga_rewrite', $ga, time() + 604800, '/', '.'.$domain);
+        } 
+    }
+}
+add_action('init', 'ga_rewrite');
+
